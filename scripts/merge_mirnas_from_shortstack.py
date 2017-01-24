fac64e0e44fb21b6b68963791ac6375b00933b07
mgalland@sils <mgalland@uva.nl>
2017-01-24 17:10:50 +0100

    now takes a list of shortstack results rather than a directory

--- scripts/merge_mirnas_from_shortstack.py
+++ scripts/merge_mirnas_from_shortstack.py
@@ -3,7 +3,8 @@
 """
 Merge all miRNA from Shortstack: we get a concatenated dataframe and a fasta file containing all miRNAs
 Usage:
-python merge_mirnas_from_shortstack.py -d [path/to/shortstack/results] -o [path/to/outfile] 
+python merge_mirnas_from_shortstack.py -d [list of shortstack result files] -o [path/to/outfile] 
+
 file hierarchy
 shortstack/
 	C32/
@@ -12,23 +13,22 @@ shortstack/
 		|-- Results.txt
 	PI127826/
 		|-- Results.txt
+
+Example: python merge_mirnas_from_shortstack.py -d shortstack/C32/Results.txt shortstack/LA4024/Results.txt 
 """
 
 ####################
 ## Import librairies
 ####################
 # import librairies
-import os
 import pandas as pd
-import re
 import argparse
-import subprocess
 
 ####################
 ## Arguments
 ####################
 parser = argparse.ArgumentParser()
-parser.add_argument("-d","--shortstackdir",type=str,help="the Shortstack directory that contains the Shortstack results.txt files")
+parser.add_argument("-l","--shortstackfiles",nargs="+",help="the Shortstack result files stored as a list")
 parser.add_argument("-o","--outtextfile",type=str,help="the tabulated text file returned as an output",default="miRNAs.txt")
 parser.add_argument("-f","--outfastafile",type=str,help="the fasta file containing miRNAs sequences returned as an output",default="miRNAs.fasta")
 args = parser.parse_args()
@@ -37,22 +37,14 @@ args = parser.parse_args()
 ## Import all ShortStack results
 ##########################################
 
-# samples
-pattern = re.compile("[A-Z+][0-9]+")
-samples = [s for s in os.listdir(args.shortstackdir) if pattern.search(s)]
-print(samples)
-
-# import ShortStack result files in a list
-shortstack_dfs = [args.shortstackdir + s + "/Results.txt" for s in samples]
-
 # read and store each ShortStack result file as a pandas dataframe
-# keep only the MajorRNA column to merge them
-dfs = [pd.read_table(f) for f in shortstack_dfs]
+dfs = [pd.read_table(f) for f in args.shortstackfiles]
 
 # select only miRNAs
 for i in range(0,len(dfs),1):
 	dfs[i] = dfs[i][dfs[i].MIRNA == "Y"]
 	dfs[i] = dfs[i][["MajorRNA"]]
+
 print("#############################")
 print("miRNA column was selected. This is how one dataframe looks like")
 print(dfs[0].head())
@@ -65,12 +57,6 @@ print("##############################")
 
 # initialize the first dataframe and merge
 df = pd.merge(dfs[0],dfs[1],on="MajorRNA",how="outer")
-print("\n")
-print("#############################")
-#print("First merge")
-#print(df.head())
-print("##############################")
-
 
 # starts to iterate and recursively merge the dfs
 for i in range(2,len(dfs)-1,1):
@@ -101,10 +87,10 @@ print("We found {0} non-redundant miRNAs".format(len(miRNAs)))
 
 # write to file
 with open("miRNAs.txt","w") as fileout1:
-	fileout1.write("id"+"\t"+"miRNA_seq")
+	fileout1.write("id"+"\t"+"miRNA_seq" + "\n")
 	for i in range(0,len(miRNAs),1):
-		fileout1.write(str(i)+"\t"+miRNAs[i]+"\n")
+		fileout1.write(str(i+1)+"\t"+miRNAs[i]+"\n")
 with open("miRNAs.fasta","w") as fileout2:
 	for i in range(0,len(miRNAs),1):
-		fileout2.write(">miRNA"+str(i)+"\n"+miRNAs[i]+"\n")
+		fileout2.write(">miRNA"+str(i+1)+"\n"+miRNAs[i]+"\n")
 
