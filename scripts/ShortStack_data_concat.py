9c3dcff5109b7a987da228a053de020e8ea59e6c
Marc Galland <m.galland@uva.nl>
2017-02-20 16:45:34 +0100

    Made some comments
    I now see the problem of putting 
    Let's say, you analyse data and you send the output to shortstack/
    Then, if my script takes as input the shortstack/ folder and list all folders in there (to retrieve sample names for instance), then it will also take I have a script that list the directory.
    To make our life easier when we share the same input data, we should keep the input folder and files as "read only". 
    Let's work on that.

--- scripts/ShortStack_data_concat.py
+++ scripts/ShortStack_data_concat.py
@@ -7,6 +7,10 @@ import glob
 # Change to the directory with your shortstack output
 os.chdir('/Users/michelle/surfdrive/Shared/trichome_team/06.results_from_xp/sRNA-Seq/20170117_srnadesc/shortstack')
 
+# here I would add a line to get the accession names. I am careful with wildcards since they don't check on anything.
+# instead, I recommend to use os.listdir (should return only shortstack accession names)
+# other option: glob.glob but with regular expression controls.
+
 # Create path to (shortstack) Result files
 path = '/Users/michelle/surfdrive/Shared/trichome_team/06.results_from_xp/sRNA-Seq/20170117_srnadesc/shortstack/*/Results.txt'
 
@@ -36,4 +40,5 @@ concat = pd.concat(list_miRNA)
 
 # The new df is saved in an output file
 concat.to_csv('/Users/michelle/surfdrive/Shared/trichome_team/06.results_from_xp/sRNA-Seq/20170117_srnadesc/shortstack/concat_miRNA.txt', index=False, sep="\t")
-
+# I would not put it in shortstack/ but rather in 06.results20170117_srnadesc/michelle/ or in 11.analysis/sRNA/based_on_results.../michelle/
+# Reason = keep input files as they are when they come out (here they come out of Snakemake "srnadesc pipeline") 
